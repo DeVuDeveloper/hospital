e87df8a031c5e4d223e69f83c63e95bc72e826ca
VuDej <drvu47@gmail.com>
2022-09-22 13:08:48 +0200

    views, routes and omniuth.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,34 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'application#home'
+  
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  # Omniauth GitHub and Google route
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
+
+  # Only Admin can see Users Index
+  resources :users, except: [:index]  
+   
+  # Patients access to web application
+  resources :patients, only: [:new, :create, :show] do
+    resources :healthcare_teams, only: [:index, :new, :create, :show]
+  end
+
+  # Customized url to filter Doctors Specialty
+  get '/select_specialty', to: 'healthcare_teams#select_specialty'
+  post '/patients/:patient_id/healthcare_teams/new', to: 'healthcare_teams#new'
+
+  # Patients can only view Doctors' Index and Show Page
+  resources :doctors, only: [:index, :show] 
+
+  # Admin privileges
+  namespace :admin do 
+    resources :users, only: [:index, :show, :edit, :update, :destroy]
+    resources :patients, only: [:edit, :update]
+    resources :doctors, only: [:edit, :update]
+    resources :healthcare_teams, only: [:edit, :update, :destroy]
+  end 
 end
