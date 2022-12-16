8e3ba8054a5cb3787b473960d6b3f66b8d72eb3d
VuDej <drvu47@gmail.com>
2022-12-16 16:05:35 +0100

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -39,6 +40,8 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  config.hosts << "up.railway.app"
+
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
